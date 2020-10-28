c418fd0474a38f0bbd20bfe45a367cd82f18486c
lazyoracle <contact@anuragsaharoy.me>
2020-10-28 11:10:01 +0530

    dummy project structure

---
+++ fakeproj/fakedir/fakemodule.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def fakefunc(fakelist: list):
+    fake_array = np.array(fakelist)
+    print(fake_array)
+    return fake_array
+
+def yaff(fakestr: str):
+    print(fakestr)
+    return fakestr
