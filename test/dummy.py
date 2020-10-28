dcfad0ad4e79d3a0790ac844bd9ad64d5f7446fe
lazyoracle <contact@anuragsaharoy.me>
2020-10-28 13:34:32 +0530

    cleanup tests

--- test/dummy.py
+++
@@ -1,6 +0,0 @@
-from fakeproj.fakedir.fakemodule import fakefunc
-
-def dummyfunc() -> int:
-    mylist = [1, 2, 3, 4, 5]
-    myarray = fakefunc(mylist)
-    return len(myarray)
--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,4 +1,9 @@
-import dummy
+from fakeproj.fakedir.fakemodule import fakefunc
+
+def dummyfunc() -> int:
+    mylist = [1, 2, 3, 4, 5]
+    myarray = fakefunc(mylist)
+    return len(myarray)
 
 def test_dummy_func() -> None:
-    assert dummy.dummyfunc() == 5
+    assert dummyfunc() == 5
