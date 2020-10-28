8b2f62bd2725863e56be01857aa8b4b53baaf3d5
lazyoracle <contact@anuragsaharoy.me>
2020-10-28 11:10:50 +0530

    build

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='fakeproj',
+    version='1.0rc',
+    packages=[
+        'fakeproj',
+        'fakeproj/fakedir'
+    ],
+    long_description=open('README.md').read(),
+    install_requires=[
+        'numpy',
+        'pytest',
+        'pytest-cov',
+        'radon'
+    ]
+)
