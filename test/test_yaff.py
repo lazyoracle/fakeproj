b9ed63c660a5157a047de753f90fe36274bf9022
lazyoracle <contact@anuragsaharoy.me>
2020-10-28 16:02:53 +0000

    black formatting

--- test/test_yaff.py
+++ test/test_yaff.py
@@ -4,7 +4,7 @@ module to test yet another fake functions
 
 from fakeproj.fakedir.fakemodule import yaff
 
+
 def test_yaff() -> None:
-    """test yaff
-    """
+    """test yaff"""
     assert yaff("foo") == "foo"
