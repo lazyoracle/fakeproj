cdf8e962f5b7bcb6d101a0d2718bf0b88826f3a0
lazyoracle <contact@anuragsaharoy.me>
2020-10-28 16:11:29 +0530

    complete tests

---
+++ test/test_yaff.py
@@ -0,0 +1,10 @@
+"""
+module to test yet another fake functions
+"""
+
+from fakeproj.fakedir.fakemodule import yaff
+
+def test_yaff() -> None:
+    """test yaff
+    """
+    assert yaff("foo") == "foo"
