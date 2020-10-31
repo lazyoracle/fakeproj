6c768bfc62d4124831f612e3270a98e590d5f264
lazyoracle <contact@anuragsaharoy.me>
2020-10-31 21:20:26 +0000

    add pytha_quad with parametric tests

--- fakeproj/gooddir/goodmodule.py
+++ fakeproj/gooddir/goodmodule.py
@@ -39,3 +39,22 @@ def recp_sum(numbers: Vector) -> float:
         if i != 0:
             result += 1.0 / i
     return result
+
+
+def pytha_quad(numbers: Vector) -> bool:
+    """check if a list is pythagorean quadruplet
+
+    Parameters
+    ----------
+    numbers : list(float)
+        list of 4 numbers
+
+    Returns
+    -------
+    bool
+        if it is a pythagorean quadruplet
+    """
+    assert len(numbers) == 4, "Should be a list of 4 numbers"
+
+    sq_numbers = [i * i for i in numbers[:-1]]
+    return sum(sq_numbers) == (numbers[-1] * numbers[-1])
---
+++ test/test_pytha_quad.py
@@ -0,0 +1,42 @@
+"""module for testing pythagorean quadruplets function
+"""
+
+import pytest
+from fakeproj.gooddir.goodmodule import pytha_quad
+from typing import List
+
+
+@pytest.mark.goodmodule
+@pytest.mark.parametrize(
+    "numbers",
+    [
+        [1, 2, 2, 3],
+        [2, 3, 6, 7],
+        [4, 4, 7, 9],
+        [1, 4, 8, 9],
+        [6, 6, 7, 11],
+        [2, 6, 9, 11],
+    ],
+)
+def test_pytha_quad(numbers: List[int]) -> None:
+    """pass test for pythagorean quads
+
+    Parameters
+    ----------
+    numbers : List[int]
+        set of 4 numbers
+    """
+    assert pytha_quad(numbers)
+
+
+@pytest.mark.goodmodule
+@pytest.mark.parametrize("numbers", [[1, 1, 1, 1], [1, 2, 3, 4], [5, 6, 7, 8]])
+def test_not_pytha_quad(numbers: List[int]) -> None:
+    """fail test for pythagorean quads
+
+    Parameters
+    ----------
+    numbers : List[int]
+        set of 4 numbers
+    """
+    assert not pytha_quad(numbers)
