fb63f5f1149ce72978410eb261eb11584ab84b99
lazyoracle <contact@anuragsaharoy.me>
2020-11-23 20:59:27 +0000

    build with setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,27 @@
 setup file for installing package
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="fakeproj",
-    version="1.0rc",
+    version="1.0rc4",
+    url="https://github.com/lazyoracle/fakeproj",
+    author="Anurag Saha Roy",
+    author_email="anurag.roni@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    include_package_data=True,
     packages=["fakeproj", "fakeproj/fakedir", "fakeproj/gooddir", "fakeproj/slowdir"],
+    description="Mostly useless code to demonstrate Python DevOps tools",
     long_description=open("README.md").read(),
-    install_requires=["numpy==1.19.2"],
+    long_description_content_type="text/markdown",
+    install_requires=["numpy"],
+    python_requires=">=3.6",
 )
